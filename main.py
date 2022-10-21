7ba5362934fd62e7c57f8cc3b39bdd4239007ffc
Mehmet Kahya <84154488+mehmetkahya0@users.noreply.github.com>
2022-10-21 23:43:00 +0300

    Update main.py
    English language is supported now!

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 #Mehmet Kahya#
-# Kendi Arayüzünde Çalışır! #
-#version 0.5
+#v1
 from tkinter import *
 import tkinter as tk
 from tkinter import messagebox
@@ -11,7 +10,7 @@ import chime
 
 window = Tk()
 
-window.title("Mehmet Python Sayaç")
+window.title("Timer")
 window.geometry("400x200")
 window.configure(bg='white')
 
@@ -22,14 +21,14 @@ L3 = Label(window, text="Mehmet Kahya", bg="white",font=("Calibri",10))
 L3.place(x=150,y=170)
 
 #Dakika UI
-L1 = Label(window, text="Dakika: ",font=("Calibri",10,"bold"))
+L1 = Label(window, text="Minute: ",font=("Calibri",10,"bold"))
 L1.place(x=10,y=20)
 
 E1 = Entry(window, bd =5, textvariable= in_dakika)
 E1.place(x=55,y=20)
 
 #Saniye UI
-L2 = Label(window, text="Saniye: ",font=("Calibri",10,"bold"))
+L2 = Label(window, text="Seconds: ",font=("Calibri",10,"bold"))
 L2.place(x=10,y=50)
 
 E2 = Entry(window, bd =5, textvariable= in_saniye)
@@ -44,17 +43,17 @@ def SystemActivate():
     dakika_saniye = int(dakika__convert+saniye)
 
     time.sleep(dakika_saniye)
-    print("zaman doldu")
+    print("time is up!")
 
     chime.theme('zelda')
     chime.error()()
 
-    L3 = Label(window, text="{} saniyelik sayaç bitti!".format(dakika_saniye), font="C:/Windows/Fonts/ARLRDBD.TTF")
+    L3 = Label(window, text="{} second timer expired!".format(dakika_saniye), font="C:/Windows/Fonts/ARLRDBD.TTF")
     L3.place(x=40,y=110)
     
-    messagebox.showwarning("showwarning", "SAYAÇ BİTTİ")
+    messagebox.showwarning("showwarning", "Counter is over!")
 
-B = Button(window, text ="Başlat", command = SystemActivate , width=15, height=1)
+B = Button(window, text ="Start", command = SystemActivate , width=15, height=1)
 B.place(x=60,y=80)
 
 window.mainloop()
